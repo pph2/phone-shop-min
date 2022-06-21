08f5a48e0dcbe4cc1b048c1c36daaf0c54735ef7
longhai <2510595242@qq.com>
2022-06-21 14:26:09 +0800

    更新进度

--- src/constant/err.type.js
+++ src/constant/err.type.js
@@ -39,4 +39,49 @@ module.exports = {
     message: '无效的token',
     result: '',
   },
+  hasNotAdminPermission: {
+    code: '10103',
+    message: '没有管理员权限',
+    result: '',
+  },
+  fileUploadError: {
+    code: '10201',
+    message: '商品图片上传失败',
+    result: '',
+  },
+  unSupportedFileType: {
+    code: '10202',
+    message: '不支持的文件格式',
+    result: '',
+  },
+  goodsFormatError: {
+    code: '10203',
+    message: '商品参数格式错误',
+    result: '',
+  },
+  publishGoodsError: {
+    code: '10204',
+    message: '发布商品失败',
+    result: '',
+  },
+  invalidGoodsID: {
+    code: '10205',
+    message: '无效的商品id',
+    result: '',
+  },
+  cartFormatError: {
+    code: '10301',
+    message: '购物车数据格式错误',
+    result: '',
+  },
+  addrFormatError: {
+    code: '10401',
+    message: '地址数据格式错误',
+    result: '',
+  },
+  orderFormatError: {
+    code: '10501',
+    message: '订单数据格式错误',
+    result: '',
+  },
 }
---
+++ src/model/goods.model.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize')
+const seq = require('../db/seq')
+
+const Goods = seq.define(
+  'ts_goods',
+  {
+    goods_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      comment: '商品名称'
+    },
+    goods_price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      comment: '商品名称'
+    },
+    goods_num: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: '商品库存',
+    },
+    goods_img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      comment: '商品图片url',
+    }
+  },
+  {
+    paranoid: true,
+  }
+);
+
+// Goods.sync({ force: true })
+
+module.exports = Goods
